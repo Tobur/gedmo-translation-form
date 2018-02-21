909b646152fd227841ded8809ffe8bb5230efb43
Alexandr Sharamko <sharamko@zfort.com>
2018-02-21 10:50:22 +0200

    Added configuration

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GedmoTranslationFormBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        return new TreeBuilder();
+    }
+}
