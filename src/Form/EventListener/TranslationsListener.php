3c02575bcea10c3ef7e07147ae1d38a013b9fa47
Alexandr Sharamko <sharamko@zfort.com>
2018-02-21 10:36:31 +0200

    Initial commit

---
+++ src/Form/EventListener/TranslationsListener.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace GedmoTranslationFormBundle\Form\EventListener;
+
+use GedmoTranslationFormBundle\Form\Type\TranslationType;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+use Gedmo\Translatable\Translatable;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\Exception\InvalidConfigurationException;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class TranslationsListener implements EventSubscriberInterface
+{
+    /**
+     * @var array
+     */
+    protected $locales;
+
+    /**
+     * @var string
+     */
+    protected $defaultLocale;
+
+    /**
+     * @param array $locales
+     * @param string $defaultLocale
+     */
+    public function __construct(array $locales, $defaultLocale)
+    {
+        $this->locales = $locales;
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            FormEvents::PRE_SET_DATA => 'preSetData',
+            FormEvents::POST_SUBMIT => 'postSubmit',
+        ];
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function preSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $options = $form->getConfig()->getOptions();
+        $entity = $form->getRoot()->getData();
+        $propertyName = $form->getPropertyPath()->__toString();
+        foreach ($this->locales as $locale) {
+            if (!method_exists($entity, 'getTranslations')) {
+                throw new InvalidConfigurationException(
+                    'Please specify getTranslations method. Follow gide by Personal translations in Gedmo.'
+                );
+            }
+            $translations = $entity->getTranslations();
+            $emptyData = '';
+            foreach ($translations as $translation) {
+                if ($translation->getLocale() === $locale && $translation->getField() === $propertyName) {
+                    $emptyData = $translation->getContent();
+                    break;
+                }
+            }
+
+            if (isset($options[TranslationType::FIELD_OPTIONS]['attr'])) {
+                $attribute = $options[TranslationType::FIELD_OPTIONS]['attr'];
+            } else {
+                $attribute = [];
+            }
+
+            $attribute['is_active_tab'] = $locale === $this->defaultLocale ? true : false;
+            $attribute['unique'] = uniqid($locale);
+
+            $form->add(
+                $locale.'_content',
+                $options[TranslationType::TYPE],
+                array_merge(
+                    [
+                        'label' => $locale,
+                        'data' => $emptyData
+                    ],
+                    $options[TranslationType::FIELD_OPTIONS],
+                    [
+                        'attr' => $attribute
+                    ]
+                )
+            );
+        }
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function postSubmit(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $entity = $form->getRoot()->getData();
+        $propertyName = $form->getPropertyPath()->__toString();
+        foreach ($this->locales as $locale) {
+            $data = $form->get($locale . '_content')->getData();
+            $needTranslation = null;
+            if (!method_exists($entity, 'getTranslations')) {
+                throw new InvalidConfigurationException(
+                    'Please specify getTranslations method. Follow gide by Personal translations in Gedmo.'
+                );
+            }
+            foreach ($entity->getTranslations() as $translation) {
+                if ($translation->getLocale() === $locale && $translation->getField() === $propertyName) {
+                    $needTranslation = $translation;
+                    $needTranslation->setContent($data);
+                    break;
+                }
+            }
+
+            if ($needTranslation === null) {
+                $className = sprintf('%sTranslation', get_class($entity));
+                $vacancyTranslation = new $className($locale, $propertyName, $data);
+
+                if (!method_exists($entity, 'addTranslation')) {
+                    throw new InvalidConfigurationException(
+                        'Please specify addTranslation method. Follow gide by Personal translations in Gedmo.'
+                    );
+                }
+
+                $entity->addTranslation($vacancyTranslation);
+                $vacancyTranslation->setObject($entity);
+            }
+        }
+    }
+}
---
+++ src/Form/Type/TranslationType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace GedmoTranslationFormBundle\Form\Type;
+
+use GedmoTranslationFormBundle\Form\EventListener\TranslationsListener;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TranslationType extends AbstractType
+{
+    const TYPE = 'type';
+    const FIELD_OPTIONS = 'field_options';
+    const ENTITY = 'entity';
+
+    /**
+     * @var TranslationsListener
+     */
+    protected $translationListener;
+
+    /**
+     * @param TranslationsListener $translationsListener
+     */
+    public function __construct(TranslationsListener $translationsListener)
+    {
+        $this->translationListener = $translationsListener;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addEventSubscriber($this->translationListener);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            static::TYPE => null,
+            static::FIELD_OPTIONS => [],
+            'mapped' => false
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'translatable';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'translatable';
+    }
+}
